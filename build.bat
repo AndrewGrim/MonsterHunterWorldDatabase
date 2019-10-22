7eacef1fac9518edf4e1cf86d018eb7f438aa540
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2019-10-22 21:01:50 +0100

    change build script to build both 32 and 64 bit version

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-del /f MHWDB.exe & pyinstaller -w -F -i images\Nergigante.ico src\Application.py & move dist\Application.exe ..\MonsterHunterWorld & ren Application.exe MHWDB.exe
+del /f MHWDB-x64.exe & pyinstaller -w -F -i images\Nergigante.ico src\Application.py & move dist\Application.exe ..\MonsterHunterWorld & ren Application.exe MHWDB-x64.exe &
+del /f MHWDB-x86.exe & %PYTHON32%\scripts\pyinstaller.exe -w -F -i images\Nergigante.ico src\Application.py & move dist\Application.exe ..\MonsterHunterWorld & ren Application.exe MHWDB-x86.exe
